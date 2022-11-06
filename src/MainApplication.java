a6cfdfe45b4bd45f23d70351d05ad3b01c00e6b5
Hwang Ji Hun <hhhhicode@gmail.com>
2022-11-06 18:27:25 +0900

    예상대진표

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -2,9 +2,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
 
-        N개의최소공배수 sol = new N개의최소공배수();
+        예상대진표 sol = new 예상대진표();
 
-        int answer = sol.solution(new int[]{1,2,3});
+        int answer = sol.solution(8,4,7);
 
         System.out.println("answer = " + answer);
     }
--- src/예상대진표.java
+++ src/예상대진표.java
@@ -1,4 +1,5 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class 예상대진표 {
@@ -6,9 +7,29 @@ public class 예상대진표 {
     {
         int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        List<Integer> aRoute = getRoute(n, a);
+        List<Integer> bRoute = getRoute(n, b);
+        for (int i = 0; i < aRoute.size(); i++) {
+            if (aRoute.get(i).equals(bRoute.get(i))) {
+                answer = i + 1;
+                break;
+            }
+        }
 
         return answer;
+    }
+
+    private List<Integer> getRoute(int mans, int num) {
+        List<Integer> routeList = new ArrayList<>();
+        while (mans != 1) {
+            if (num % 2 != 0) {
+                num += 1;
+            }
+            routeList.add(num);
+            num /= 2;
+            mans /= 2;
+        }
+
+        return routeList;
     }
 }
