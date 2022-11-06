5aaea84961e4bfc95f965a5ef459e9c4f055d677
Hwang Ji Hun <hhhhicode@gmail.com>
2022-11-07 03:43:17 +0900

    lv2_행렬의 곱셈

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -2,9 +2,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
 
-        lv2_HIndex sol = new lv2_HIndex();
+        lv2_행렬의곱셈 sol = new lv2_행렬의곱셈();
 
-        int answer = sol.solution(new int[]{4, 4, 4});
+        int[][] answer = sol.solution(new int[][]{{2,3,2}, {4,2,4}, {3,1,4}}, new int[][]{{5,4,3}, {2,4,1}, {3,1,1}});
 
         System.out.println("answer = " + answer);
     }
---
+++ src/lv2_행렬의곱셈.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/42747
+public class lv2_행렬의곱셈 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = matrixMul(arr1, arr2);
+
+        return answer;
+    }
+
+    private int[][] matrixMul(int[][] arr1, int[][] arr2) {
+        int[][] arr3 = new int[arr1.length][arr2[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr2.length; k++) {
+                    arr3[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return arr3;
+    }
+}
+
+//MxN * NxO => MxO
+//MxO에 대해 표현한다고 생각하면 된다.
+//  for(M)
+//      for(N)
+//          for(N) -> N이 변하며 계산하는 부분
+
