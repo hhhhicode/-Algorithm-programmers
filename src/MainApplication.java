7dee02a082292897368d42b822b2c2571d5b1a7a
Hwang Ji Hun <hhhhicode@gmail.com>
2022-11-06 19:08:30 +0900

    lv2_점프와 순간 이동

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -2,9 +2,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
 
-        예상대진표 sol = new 예상대진표();
+        점프와순간이동 sol = new 점프와순간이동();
 
-        int answer = sol.solution(8,4,7);
+        int answer = sol.solution(1000000000);
 
         System.out.println("answer = " + answer);
     }
---
+++ src/점프와순간이동.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12980
+public class 점프와순간이동 {
+    public int solution(int n) {
+        int ans = getCount(n);
+
+        return ans;
+    }
+
+    private int  getCount(int n) {
+        int count = 0;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+// 5000 -> 2500 -> 1250 -> 625 -> 624++ ->
+// 312 -> 156 -> 78 -> 39 -> 38++ ->
+// 19 -> 18++ ->
+// 9 -> 8++ ->
+// 4 -> 2 -> 1 -> 0++
+//=> 5
