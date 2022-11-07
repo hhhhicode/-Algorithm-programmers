668eb90cb95b85e221ddaa433e622e06ac6688c4
Hwang Ji Hun <hhhhicode@gmail.com>
2022-11-07 20:20:40 +0900

    lv2_n의2제곱배열자르기

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -2,9 +2,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
 
-        lv2_위장 sol = new lv2_위장();
+        lv2_n의2제곱배열자르기 sol = new lv2_n의2제곱배열자르기();
 
-        int answer = sol.solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}});
+        int[] answer = sol.solution(4,7,14);
 
         System.out.println("answer = " + answer);
     }
---
+++ src/lv2_n의2제곱배열자르기.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/87390
+public class lv2_n의2제곱배열자르기 {
+    public int[] solution(int n, long left, long right) {
+
+        List<Integer> list = new ArrayList<>();
+        for (long num = left; num <= right; num++) {
+            list.add((int) (Math.max(num / n, num % n) + 1));
+        }
+
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
+
+//1 row 에 n개
+//0 1 2
+//3 4 5
+//6 7 8
+
+//left == (left/n, left%n)
+// => i*n + j == num
+
+//i행의 i까지는 i이다. i+1부터는 j를 따른다.
+
+//3 = (1,4) 4
+//4 = (2,1) 2
+//5 = (2,2) 2
+//6 = (2,3) 3
+//7 = (2,4) 4
+//8 = (3,1) 3
+//9 = (3,2) 3
+//10= (3,3) 3
+//11= (3,4) 4
+//12= (4,1) 4
+//13= (4,2) 4
+//14= (4,3) 4
