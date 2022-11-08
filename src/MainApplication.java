522d5052b32311a81e6ad482d070db65b7911408
Hwang Ji Hun <hhhhicode@gmail.com>
2022-11-08 17:34:24 +0900

    lv2_기능개발

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -2,9 +2,9 @@ public class MainApplication {
 
     public static void main(String[] args) {
 
-        lv2_n의2제곱배열자르기 sol = new lv2_n의2제곱배열자르기();
+        lv2_기능개발 sol = new lv2_기능개발();
 
-        int[] answer = sol.solution(4,7,14);
+        int[] answer = sol.solution(new int[]{93, 30, 55}, new int[]{1, 30, 5});
 
         System.out.println("answer = " + answer);
     }
---
+++ src/lv2_기능개발.java
@@ -0,0 +1,46 @@
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/42586
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class lv2_기능개발 {
+    public int[] solution(int[] progresses, int[] speeds) {
+
+        int[] addArr = new int[speeds.length];
+
+        Queue<Integer> progressQueue = new ArrayDeque<>();
+        setQueue(progressQueue, progresses);
+
+        List<Integer> integerList = new ArrayList<>();
+        int j = 0;
+        while (!progressQueue.isEmpty()) {
+            int count = 0;
+
+            //얼마나 했냐?
+            for (int i = 0; i < speeds.length; i++) {
+                addArr[i] += speeds[i];
+            }
+
+            //오늘 몇 개 배포하냐?
+            while (!progressQueue.isEmpty() && progressQueue.peek() + addArr[j] >= 100) {
+                count++;
+                j++;
+                progressQueue.poll();
+            }
+
+            if (count != 0) {
+                integerList.add(count);
+            }
+        }
+        System.out.println("integerList = " + integerList);
+
+        return integerList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private void setQueue(Queue<Integer> queue, int[] arr) {
+        for (int value : arr) {
+            queue.add(value);
+        }
+    }
+}
